f0c79e7b6577feeb9cd988aa79e5313f265cb551
Dayna Clare <daycanimate@gmail.com>
2023-11-10 20:14:46 -0600

    Completed excerise 2 in app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 "use strict";
-const rqst = new XMLHttpRequest();
-
-function getHeroes(){
-    if (rqst.readyState === XMLHttpRequest.DONE){
-        if (rqst.status === 200){
-            let reply = rqst.responseText;
-            alert(reply);
-        } else{
-            alert('There was a problem with the request.')
-        }
-    }
-}
 
 document.addEventListener('DOMContentLoaded', () => {
     const searchbtn = document.getElementById('search');
     let heroesUrl = "superheroes.php";
+    const rqst = new XMLHttpRequest();
 
     console.log(searchbtn);
     
-    searchbtn.addEventListener('click', function(el) {  
+    searchbtn.addEventListener('click', function(el){  
         console.log("Guau");
+        el.preventDefault();
 
-        rqst.onreadystatechange = getHeroes();
+        rqst.onreadystatechange = function (){
+            if (rqst.readyState === XMLHttpRequest.DONE){
+                if (rqst.status === 200){
+                    let reply = rqst.responseText;
+                    alert(reply);
+                } else{
+                    alert('There was a problem with the request.')
+                }
+            }
+        };
+
         rqst.open('GET', heroesUrl);
         rqst.send();
     });
